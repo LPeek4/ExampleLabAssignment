70008ba236e878025761d8b8073d150562fe5c03
11701 <logan.peek@vtsd.com>
2022-09-12 07:58:20 -0400

    Test Commit

---
+++ DataTypesExamples.java
@@ -0,0 +1,20 @@
+// Creates a single line comment
+
+/*
+    This is a multi-line comment
+    All the lines between the slashes and stars will be ignored
+ */
+
+public class DataTypesExamples {
+    public static void main(String args []){
+        // Variable syntax: dataType narName = value;
+        int age = 200;
+        // Character values must us a single quote
+        char favLetter= 'Z';
+        // String values must be double quotes
+        String bestGame = "chess";
+
+
+    }
+
+}
--- OurFirstClass.java
+++ OurFirstClass.java
@@ -1,9 +1,9 @@
-/*
-
-Here we Goooo - Disney
-
-*/
- */
 
 public class OurFirstClass {
+  public static void main(String args[]){
+   System.out.print("Hello World");
+
+  }
+
+
 }
